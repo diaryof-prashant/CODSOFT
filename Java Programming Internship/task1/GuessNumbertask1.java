d27ce25fb24766487047c5cb97a0bfd86c5a8718
Prashant Mishra <65122711+diaryof-prashant@users.noreply.github.com>
2023-08-22 00:26:11 +0530

    Update GuessNumbertask1.java

--- Java Programming Internship/task1/GuessNumbertask1.java
+++ Java Programming Internship/task1/GuessNumbertask1.java
@@ -9,26 +9,26 @@ public class GuessNumbertask1
         int guessnum = random.nextInt(101);
         int t=5;
         Scanner scan = new Scanner(System.in);
-        System.out.println("I selected a number between 0 and 100.\n Lets see if you can correctly guess the number.\n You have got 5 tries to guess the number.\n Lets begin.\n");
+        System.out.println("A number is selected between 0 and 100.\nLets see if you can correctly guess the number.\nYou have got 5 tries to guess the number.\nLets begin.\n");
 
         while(t>0) {
 
             System.out.println("Enter your guess : ");
             int userinp = scan.nextInt();
             if (userinp == guessnum) {
-                System.out.println("You guessed the number.\n You WIN!");
+                System.out.println("You guessed the number.\nYou WIN!");
                 guessed=true;
                 break;
             } else if (userinp > guessnum) {
-                System.out.println("Your guess is too high.\n You have " + (t - 1) + " tries left");
+                System.out.println("Your guess is too high.\nYou have " + (t - 1) + " tries left");
                 t--;
             } else {
-                System.out.println("Your guess is too low.\n You have " + (t - 1) + " tries left");
+                System.out.println("Your guess is too low.\nYou have " + (t - 1) + " tries left");
                 t--;
             }
         }
         if(guessed==false){
-                System.out.println("You ran out of tries.\n You LOSE!\n");
+                System.out.println("You ran out of tries.\nYou LOSE!\n");
                 System.out.println("Correct number was "+ guessnum + ".");
         }
     }
